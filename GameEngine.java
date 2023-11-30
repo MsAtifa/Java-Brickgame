1954095eb1aacc92060f4c8d5fec6b24f588cdf2
MsAtifa <152516802+MsAtifa@users.noreply.github.com>
2023-12-01 03:19:43 +0800

    GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -1,13 +1,18 @@
 package brickGame;
+import javafx.application.Platform;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class GameEngine {
 
     private OnAction onAction;
     private int fps = 15;
-    private Thread updateThread;
-    private Thread physicsThread;
     public boolean isStopped = true;
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private long time = 0;
 
     public void setOnAction(OnAction onAction) {
         this.onAction = onAction;
@@ -20,86 +25,43 @@ public class GameEngine {
         this.fps = (int) 1000 / fps;
     }
 
-    private synchronized void Update() {
-        updateThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!updateThread.isInterrupted()) {
-                    try {
-                        onAction.onUpdate();
-                        Thread.sleep(fps);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        updateThread.start();
+    private void update() {
+        scheduler.scheduleAtFixedRate(() -> Platform.runLater(onAction::onUpdate), 0, fps, TimeUnit.MILLISECONDS);
     }
 
-    private void Initialize() {
-        onAction.onInit();
+    private void initialize() {
+        Platform.runLater(onAction::onInit);
     }
 
-    private synchronized void PhysicsCalculation() {
-        physicsThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!physicsThread.isInterrupted()) {
-                    try {
-                        onAction.onPhysicsUpdate();
-                        Thread.sleep(fps);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        physicsThread.start();
-
+    private void physicsCalculation() {
+        scheduler.scheduleAtFixedRate(() -> {
+            Platform.runLater(onAction::onPhysicsUpdate);
+            onAction.onTime(time++);
+        }, 0, fps, TimeUnit.MILLISECONDS);
     }
+
+    
 
     public void start() {
-        time = 0;
         Initialize();
         Update();
         PhysicsCalculation();
-        TimeStart();
         isStopped = false;
     }
 
     public void stop() {
         if (!isStopped) {
             isStopped = true;
-            updateThread.stop();
-            physicsThread.stop();
-            timeThread.stop();
+            scheduler.shutdown();
+            try {
+                if (!scheduler.awaitTermination(1, TimeUnit.SECONDS)) {
+                    scheduler.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-    private long time = 0;
-
-    private Thread timeThread;
-
-    private void TimeStart() {
-        timeThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    while (true) {
-                        time++;
-                        onAction.onTime(time);
-                        Thread.sleep(1);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        timeThread.start();
-    }
-
 
     public interface OnAction {
         void onUpdate();
