512f6cab09bfe2a01b6b680ecffd7dab32e67aef
MsAtifa <152516802+MsAtifa@users.noreply.github.com>
2023-12-12 18:55:42 +0800

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.brickgame {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.logging;
-
-
-    opens com.example.brickgame to javafx.fxml;
-    exports com.example.brickgame;
-}
